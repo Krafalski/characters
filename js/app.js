6e9497d5c55850e461ca9065aad751133135cf18
Karolin Rafalski <kr2174@caa.columbia.edu>
2019-03-17 00:37:54 -0400

    css cleanup

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
+/* global dictionary loadRound dictionaryModal event animateWrong animateCorrect */
+/* exported score  dictionarySortByName reformatNameForModal */
 let playArray = []
-let name = ''
-let character = ''
 let inPlay = {}
 let playPosition = 0
 let score = 0
 let wrongGuesses = {}
 
 class Character {
-  constructor (name, character){
+  constructor (name, character) {
     this.character = character
     this.key = name
     this.name = this.reformatName(name)
@@ -27,29 +27,36 @@ class Character {
   }
   checkInput (input) {
     this.play = input
-    this.correct = (input === this.character) ? true : false  
-    if(this.correct) score ++ 
+    if (this.key === 'smartQuote') {
+      if (input === "'" || input === '"') {
+        score++
+        this.correct = true
+      }
+    } else {
+      this.correct = (input === this.character)
+      if (this.correct) score++
+    }
   }
 }
 
 const reformatNameForModal = name => {
-    const rename = name.split('')
-    const testForCaps = /[A-Z]/
-    const newName = rename.map(l => {
-      if (testForCaps.test(l)) {
-        l = ` ${l.toLowerCase()}`
-      }
-      return l
-    })
-    return newName.join('')
+  const rename = name.split('')
+  const testForCaps = /[A-Z]/
+  const newName = rename.map(l => {
+    if (testForCaps.test(l)) {
+      l = ` ${l.toLowerCase()}`
+    }
+    return l
+  })
+  return newName.join('')
 }
-
-
 
 // https://stackoverflow.com/questions/5467129/sort-javascript-object-by-key
 const dictionarySortByName = (unsortedDictionary) => {
   const sortedDictionary = {}
-  Object.keys(unsortedDictionary).sort().forEach(key => sortedDictionary[key] = unsortedDictionary[key])
+  Object.keys(unsortedDictionary).sort().forEach(key => {
+    sortedDictionary[key] = unsortedDictionary[key]
+  })
   return sortedDictionary
 }
 
@@ -59,25 +66,24 @@ const dictionarySortByCharacter = (unsortedDicitonary) => {
   for (let char in unsortedDicitonary) {
     sortable.push([char, unsortedDicitonary[char]])
   }
-  sortable.sort((a,b) => {
+  sortable.sort((a, b) => {
     return a[1] - b[1]
   })
   const sortedDictionary = {}
-    sortable.forEach(charObj => {
-      sortedDictionary[charObj[0]] = charObj[1]
-    })
+  sortable.forEach(charObj => {
+    sortedDictionary[charObj[0]] = charObj[1]
+  })
   return sortedDictionary
 }
-
 
 // return valid play
 const allowPlay = keyPress => {
   const letterCheck = /[a-z0-9]/i
-  let specialKeys = ['Escape' , 'CapsLock', 'Shift', 'Control', 'Alt', 'Meta', 'Backspace', 'Enter']
+  let specialKeys = ['Escape', 'CapsLock', 'Shift', 'Control', 'Alt', 'Meta', 'Backspace', 'Enter']
   if (letterCheck.test(keyPress)) {
     return false
   }
-  for (let press of specialKeys ) {
+  for (let press of specialKeys) {
     if (press === keyPress) {
       return false
     }
@@ -101,8 +107,9 @@ const newGame = () => {
 // fisher-yates shuffle
 // https://bost.ocks.org/mike/shuffle/
 const shuffle = array => {
-  let m = array.length, t, i
-
+  let m = array.length
+  let t
+  let i
   // While there remain elements to shuffle…
   while (m) {
     // Pick a remaining element…
@@ -113,39 +120,31 @@ const shuffle = array => {
     array[m] = array[i]
     array[i] = t
   }
-  return array;
+  return array
 }
 
-
 const nextRound = () => {
-  if(playArray.length - 1 > playPosition) {
-    playPosition ++
+  if (playArray.length - 1 > playPosition) {
+    playPosition++
     inPlay = playArray[playPosition]
     loadRound()
-  } else {  
+  } else {
     // show wrong dictionary
     dictionaryModal('wrong')
     newGame()
-  } 
+  }
 }
 
 const checkPair = () => {
   if (allowPlay(event.key)) {
     inPlay.checkInput(event.key)
-    if(!inPlay.correct) {
+    if (!inPlay.correct) {
       wrongGuesses[inPlay.name] = inPlay.character
-      // wrong animation
       animateWrong()
     } else {
-      //right animation
       animateCorrect()
     }
-
     setTimeout(nextRound, 1000)
     setTimeout(loadRound, 1001)
-    
   }
 }
-
-
-
--- js/ui.js
+++ js/ui.js
@@ -1,107 +1,120 @@
+/* global $ inPlay playArray score playPosition dictionary wrongGuesses */
 const loadRound = () => {
-    let $character = $('.character')
-    let $name = $('.name')
-    $character.html(`<h4>${inPlay.character}</h4>`)
-      .css('opacity', 0)
-    $name.html(`<h4>${inPlay.name}</h4>`)
-    $('.correct').text(score)
-    $('.remaining > span').text(playArray.length - playPosition)
-  }
-  
-  const animateCorrect = () => {
-    $('.character').css({'opacity': 1, 'color': 'turquoise'})
-  }
+  let $character = $('.character')
+  let $name = $('.name')
+  $character.html(`<h4>${inPlay.character}</h4>`)
+    .css('opacity', 0)
+  $name.html(`<h4>${inPlay.name}</h4>`)
+  $('.correct').text(score)
+  $('.remaining > span').text(playArray.length - playPosition)
+}
 
-  const animateWrong= () => {
-    $('.character').css({'opacity': 1, 'color': 'firebrick'})
-  }
-  const openModal = () => {
-    $('.modal').show().on('click', () => {
-      $('.modal').hide()
-    })
-  }
-  const aboutModal = () => {
-    $('.modal-content').empty().html(`
-      <h2>About</h2>
-      <p>This project is to help people learn the keyboard character keys they may not be used to using. Espcially, when they just start coding</p>
-    `)
-    openModal()
-  }
-  const howModal = () => {
-    $('.modal-content').empty().html(`
-    <h2>How to Play</h2>
-    <p>The name of the character appears in black. Just type the character. Don't forget to use the shift key, as needed!</p>
-    <p>Charcters can have multiple names, so you can enter the same character multiple times</p>
-    <p>For characters that are pairs, like parenthesis (), only type the opening one.</p>
-    <p>Your total correct guesses appear in the upper right</p>
-    <p>The remaining characters to guess appears on the bottom left</p>
-  `)
-    openModal()
-  }
+const animateCorrect = () => {
+  $('.character').css({ 'opacity': 1, 'color': 'turquoise' })
+}
 
-  const dictionaryModal = type => {
-    let $table
-    let $h2
-    const $content = $('.modal-content')
-    if (type === 'name' || !type) {
-      const displayDictionary = dictionarySortByName(dictionary)
-      $table = tableBuilder(displayDictionary)
-    } else if (type === 'character') {
-      const displayDictionary = dictionarySortByCharacter(dictionary)
-      $table = tableBuilder(displayDictionary)
-    } else if (type ==='wrong') {
-
-      const displayDictionary = dictionarySortByName(wrongGuesses)
-      $table = tableBuilder(displayDictionary)
-      $content.append($h2)
-    }else  {
-      const displayDictionary = dictionarySortByName(dictionary)
-      $table = tableBuilder(displayDictionary)
-    }
-    if (type===
-      'wrong'){
-      $content.empty()
-      const $h2 = $('<h2>').text('Nice job! Here are some characters to review')
-      $content.append($h2, $table)
-    } else {
-      $content.empty().append($table)
-    }
-
-    openModal()
-  }
-  const reviewModal = () => {
-    openModal()
-  }
-
-  const tableBuilder = (dictionaryInput) => {
-    const $table = $('<table>')
-    const $tableHeader = $('<tr>')
-    const $tableHeaderName = $('<th>').text('name').on('mouseenter',()=>{
-      $('.modal-content').empty()   
-      dictionaryModal('name')
-    })
-    const $tableHeaderCharacter = $('<th>').text('character').on('mouseenter',()=>{
-      $('.modal-content').empty()
-      dictionaryModal('character')
-
-    })
-    $tableHeader.append($tableHeaderName, $tableHeaderCharacter)
-    $table.append($tableHeader)
-    for (let character in dictionaryInput) {
-     const $tr =  $('<tr>').html(`
-        <td>${reformatNameForModal(character)}</td>
-        <td>${dictionaryInput[character]}</td>
-      `)
-      $table.append($tr)
-    }
-    return $table
-  }
-  $(()=>{
-    newGame()
-    $('body').keyup(checkPair)
-    $('.start').on('click', newGame)
-    $('#about').on('click', aboutModal)
-    $('#how').on('click', howModal)
-    $('#dictionary').on('click', dictionaryModal)
-    $('#review').on('click', reviewModal)
+const animateWrong = () => {
+  $('.character').css({ 'opacity': 1, 'color': 'firebrick' })
+}
+const openModal = () => {
+  $('.modal').show().on('click', () => {
+    $('.modal').hide()
   })
+}
+
+const aboutModal = () => {
+  $('.modal-content').empty().html(`
+    <h2>About</h2>
+    <p>This project is to help people learn the keyboard character keys they may not be used to using. Espcially, when they just start coding</p>
+  `)
+  openModal()
+}
+
+const howModal = () => {
+  $('.modal-content').empty().html(`
+  <h2>How to Play</h2>
+  <p>The name of the character appears in black. Just type the character. Don't forget to use the shift key, as needed!</p>
+  <p>Charcters can have multiple names, so you can enter the same character multiple times</p>
+  <p>For characters that are pairs, like parenthesis (), only type the opening one.</p>
+  <p>Your total correct guesses appear in the upper right</p>
+  <p>The remaining characters to guess appears on the bottom left</p>
+`)
+  openModal()
+}
+
+const dictionaryModal = type => {
+  let $table
+  let $h2
+  let displayDictionary
+  const $content = $('.modal-content')
+  if (type === 'name' || !type) {
+    displayDictionary = dictionarySortByName(dictionary)
+    $table = tableBuilder(displayDictionary)
+  } else if (type === 'character') {
+    displayDictionary = dictionarySortByCharacter(dictionary)
+    $table = tableBuilder(displayDictionary)
+  } else if (type === 'wrong') {
+    displayDictionary = dictionarySortByName(wrongGuesses)
+    $table = tableBuilder(displayDictionary)
+    $content.append($h2)
+  } else {
+    displayDictionary = dictionarySortByName(dictionary)
+    $table = tableBuilder(displayDictionary)
+  }
+  if (type === 'wrong') {
+    $content.empty()
+    if (Object.keys(displayDictionary).length === 0) {
+      winModal()
+    } else {
+      const $h2 = $('<h2>').text('Nice job! Here are some characters to review')
+      const $p = $('<p>').text(`
+          GOTCHA: Smart quotes are quotes that are auto-formated into a different character than the default.You can't automatically type a smart quote, and using a smart quote in code will break your code
+        `)
+      $content.append($h2, $table, $p)
+    }
+  } else {
+    $content.empty().append($table)
+  }
+  openModal()
+}
+
+const winModal = () => {
+  $('.modal-content').empty().html(`
+    <h2>🎉🎊⚡️⭐️✨🌟</h2>
+    <h2>💫 Perfect Typing! 💫</h2>
+    <h2>🌟✨⭐️⚡️🎊🎉</h2>
+  `)
+  openModal()
+}
+
+const tableBuilder = (dictionaryInput) => {
+  const $table = $('<table>')
+  const $tableHeader = $('<tr>')
+  const $tableHeaderName = $('<th>').text('name').on('mouseenter',()=>{
+    $('.modal-content').empty()
+    dictionaryModal('name')
+  })
+  const $tableHeaderCharacter = $('<th>').text('character').on('mouseenter',()=>{
+    $('.modal-content').empty()
+    dictionaryModal('character')
+  })
+  $tableHeader.append($tableHeaderName, $tableHeaderCharacter)
+  $table.append($tableHeader)
+  for (let character in dictionaryInput) {
+    const $tr = $('<tr>').html(`
+      <td>${reformatNameForModal(character)}</td>
+      <td>${dictionaryInput[character]}</td>
+    `)
+    $table.append($tr)
+  }
+  return $table
+}
+
+$(() => {
+  newGame()
+  $('body').keyup(checkPair)
+  $('.start').on('click', newGame)
+  $('#about').on('click', aboutModal)
+  $('#how').on('click', howModal)
+  $('#dictionary').on('click', dictionaryModal)
+})
